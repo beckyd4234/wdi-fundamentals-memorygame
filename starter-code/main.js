f46b292e7efabf2fa27f12f7d1501800eb326447
beckyd4234 <beckydavisphx@gmail.com>
2016-10-17 18:11:58 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,31 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+/*
+if (cardOne === cardTwo) {
+	alert ("You found a match!");
+	} else {
+	alert ("Sorry, try again.");
+	}
+
+if (cardThree === cardFour) {
+	alert ("You found a match!");
+		} else {
+	alert ("Sorry, try again.");
+		}
+		*/
+
+    var board = document.getElementById('board-game');
+
+    function createCard() {
+     for (var i=0; i<cards.length; i++) {
+       var makeCardElement = document.createElement('div');
+
+    makeCardElement.className = 'card';
+
+    board.appendChild(makeCardElement);
+    }
+    }
+    createCard(4);
